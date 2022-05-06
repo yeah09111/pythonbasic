5b344c5dc690320439bd39a502e5aa77f610c14a
yeah09111 <yeah_0911@naver.com>
2022-05-06 14:59:33 +0900

    한 자리씩 입력 > 한 번에 입력

--- BaseballGame.py
+++ BaseballGame.py
@@ -7,10 +7,9 @@ while True:
     goal_num1 = random.randrange(10)
     goal_num2 = random.randrange(10)
     goal_num3 = random.randrange(10)
-    if (goal_num1 != goal_num2) and (goal_num2 != goal_num3) and (goal_num1 != goal_num3):
+    if goal_num1 != goal_num2 != goal_num3:
         break
 
-#print(goal_num1, goal_num2, goal_num3)
 goal_num = [goal_num1, goal_num2, goal_num3]
 
 
@@ -18,11 +17,11 @@ while True:
     count_ball = 0
     count_strike = 0
 
+    inp_num=input("세 자리 숫자를 입력해 주세요(숫자 중복X). ")
+
     for i in range(3):
-        print("\n", i+1, "번째 숫자를 입력해 주세요.")
-        inp_num = int(input())
-        if inp_num in goal_num:
-            if inp_num == goal_num[i]:
+        if int(inp_num[i]) in goal_num:
+            if int(inp_num[i]) == goal_num[i]:
                 count_strike+=1
             else:
                 count_ball+=1
